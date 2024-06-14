bb3f1baf91b5079eb3ab2183110f4ebb72d51804
Rafael Belalcazar <rafael.belalcazar@numa.com.co>
2024-06-14 09:43:01 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('menu-btn').addEventListener('click', function() {
+    const nav = document.getElementById('nav');
+    // console.log(nav)
+    nav.classList.toggle('open');
+});
